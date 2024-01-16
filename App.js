252ea557d5643de237353f349ae4bb89307f5bde
Monish <monishsharma080@gmail.com>
2024-01-16 17:15:36 +0530

    set up

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NativeWindStyleSheet } from "nativewind";
+
+// npm i react-native-alarm-manager
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className="bg-slate-800 items-center justify-center flex-1  "  >
+      <Text className="text-xl text-orange-400 " >Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+NativeWindStyleSheet.setOutput({
+  default: "native",
 });
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#000',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
