8461cf2d574c590d1cbc962c01a5fd62d1636570
Monish <monishsharma080@gmail.com>
2024-01-16 18:13:15 +0530

    got safereaview to wor

--- App.js
+++ App.js
@@ -1,31 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import {   SafeAreaViewComponent, StyleSheet, Text, View } from 'react-native';
-// import { NativeWindStyleSheet } from "nativewind";
+import {     StyleSheet, Text, View } from 'react-native';
 import Header from './components/Header'
-// npm i react-native-alarm-manager
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 
 export default function App() {
   return (
-        <>
-      <View className="bg-gray-900  flex-1   "  >
-        <View className="justify-center self-left ml-5 mt-12" >
+      <SafeAreaView className="bg-gray-900  flex-1 ">
+        <View className="justify-center self-left ml-4" >
           <Header />
         </View>
-      </View>
-      </>
+      </SafeAreaView>
   );
 }
 
-// NativeWindStyleSheet.setOutput({
-//   default : "native",
-// });
-
-// const styles =t 12tyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#000',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+},
+  // container: {
+  //   flex: 1,
+  //   backgroundColor: '#000',
+  //   alignItems: 'center',
+  //   justifyContent: 'center',
+  // },
+});
