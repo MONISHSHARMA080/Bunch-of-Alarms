ca4a68e68819ef2a7c73b6ff6801666ca034a851
Monish <monishsharma080@gmail.com>
2024-01-16 17:55:16 +0530

    added headder

--- App.js
+++ App.js
@@ -1,24 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { NativeWindStyleSheet } from "nativewind";
-
+import {   SafeAreaViewComponent, StyleSheet, Text, View } from 'react-native';
+// import { NativeWindStyleSheet } from "nativewind";
+import Header from './components/Header'
 // npm i react-native-alarm-manager
 
 
 export default function App() {
   return (
-    <View className="bg-slate-800 items-center justify-center flex-1  "  >
-      <Text className="text-xl text-orange-400 " >Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+        <>
+      <View className="bg-gray-900  flex-1   "  >
+        <View className="justify-center self-left ml-5 mt-12" >
+          <Header />
+        </View>
+      </View>
+      </>
   );
 }
 
-NativeWindStyleSheet.setOutput({
-  default: "native",
-});
+// NativeWindStyleSheet.setOutput({
+//   default : "native",
+// });
 
-// const styles = StyleSheet.create({
+// const styles =t 12tyleSheet.create({
 //   container: {
 //     flex: 1,
 //     backgroundColor: '#000',
