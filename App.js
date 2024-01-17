143f6bedb06e8bfa37f6b22041d2d64d2f327abc
Monish <monishsharma080@gmail.com>
2024-01-17 11:06:47 +0530

    hbed

--- App.js
+++ App.js
@@ -19,8 +19,8 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-},
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+// },
+// });
