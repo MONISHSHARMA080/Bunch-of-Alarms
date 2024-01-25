15b41e819b06295ed1b2044e753e2ae9337cbe2b
Monish <monishsharma080@gmail.com>
2024-01-26 03:38:38 +0530

    added utils folder

---
+++ android/app/src/main/java/com/monish1/BunchofAlarms/util/AlarmReceiver.java
@@ -0,0 +1,31 @@
+package com.monish1.BunchofAlarms.util;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.icu.util.Calendar;
+
+public class AlarmReceiver extends BroadcastReceiver {
+    private AlarmManager alarmManager;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+
+        Intent alarmServiceIntent = new Intent(context, AlarmService.class);
+        alarmServiceIntent.putExtra("id", intent.getIntExtra("id", 0));
+        alarmServiceIntent.putExtra("hour", intent.getIntExtra("hour", 0));
+        alarmServiceIntent.putExtra("minute", intent.getIntExtra("minute", 0));
+        alarmServiceIntent.putExtra("title", intent.getStringExtra("title"));
+        alarmServiceIntent.putExtra("text", intent.getStringExtra("text"));
+        alarmServiceIntent.putExtra("sound", intent.getStringExtra("sound"));
+        alarmServiceIntent.putExtra("icon", intent.getStringExtra("icon"));
+        alarmServiceIntent.putExtra("soundLoop", intent.getBooleanExtra("soundLoop", true));
+        alarmServiceIntent.putExtra("vibration", intent.getBooleanExtra("vibration", true));
+        alarmServiceIntent.putExtra("notiRemovable", intent.getBooleanExtra("notiRemovable", true));
+
+        context.startForegroundService(alarmServiceIntent);
+    }
+}
