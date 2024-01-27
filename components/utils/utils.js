3a3e53bb969cbe1f16e272ec45bdb5f54b2e3d17
Monish <monishsharma080@gmail.com>
2024-01-27 09:39:07 +0530

    added thins for firebase notifiation

--- components/utils/utils.js
+++ components/utils/utils.js
@@ -2,6 +2,7 @@ import notifee, { IntervalTrigger, TriggerType, TimeUnit } from '@notifee/react-
 import * as Device from 'expo-device';
 import * as Notifications from 'expo-notifications';
 import { Alert } from 'react-native';
+import PushNotification from "react-native-push-notification";
 
 export function getCurrentTime(date){
     if (date == null){
@@ -144,8 +145,55 @@ export async function bunchofAlarm(){
 
 
 
+PushNotification.configure({
+  // (optional) Called when Token is generated (iOS and Android)
+  onRegister: function (token) {
+    console.log("TOKEN:", token);
+  },
 
+  // (required) Called when a remote is received or opened, or local notification is opened
+  onNotification: function (notification) {
+    console.log("NOTIFICATION:", notification);
 
+    // process the notification
+
+    // (required) Called when a remote is received or opened, or local notification is opened
+    // notification.finish(PushNotificationIOS.FetchResult.NoData);
+  },
+
+  // (optional) Called when Registered Action is pressed and invokeApp is false, if true onNotification will be called (Android)
+  onAction: function (notification) {
+    console.log("ACTION:", notification.action);
+    console.log("NOTIFICATION:", notification);
+
+    // process the action
+  },
+
+  // (optional) Called when the user fails to register for remote notifications. Typically occurs when APNS is having issues, or the device is a simulator. (iOS)
+  onRegistrationError: function(err) {
+    console.error(err.message, err);
+  },
+
+  // IOS ONLY (optional): default: all - Permissions to register.
+  permissions: {
+    alert: true,
+    badge: true,
+    sound: true,
+  },
+
+  // Should the initial notification be popped automatically
+  // default: true
+  popInitialNotification: true,
+
+  /**
+   * (optional) default: true
+   * - Specified if permissions (ios) and token (android and ios) will requested or not,
+   * - if not, you must call PushNotificationsHandler.requestPermissions() later
+   * - if you are not using remote notification or do not have Firebase installed, use this:
+   *     requestPermissions: Platform.OS === 'ios'
+   */
+  requestPermissions: true,
+});
 
 
 
