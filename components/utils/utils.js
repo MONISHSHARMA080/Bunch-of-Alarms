4d2dbedab3fba2abd3d29a8645a7910385ab0015
Monish <monishsharma080@gmail.com>
2024-02-20 17:02:36 +0530

    trying to change create alarm from node modules

--- components/utils/utils.js
+++ components/utils/utils.js
@@ -1,62 +1,60 @@
 // import Alarm from 'react-native-alarm-manager';
 // import react-native-alarm-manager';
-import { NativeEventEmitter, NativeModules } from 'react-native';
+import { NativeEventEmitter, NativeModules } from "react-native";
 
-export function getCurrentTime(date){
-    if (date == null){
-        date = new Date()
-    }
-const hours = date.getHours();
-const minutes = date.getMinutes();
-const formattedTime = `${hours < 10 ? '0' + hours : hours}:${minutes < 10 ? '0' + minutes : minutes}`;
-return formattedTime
+export function getCurrentTime(date) {
+  if (date == null) {
+    date = new Date();
+  }
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const formattedTime = `${hours < 10 ? "0" + hours : hours}:${minutes < 10 ? "0" + minutes : minutes}`;
+  return formattedTime;
 }
 
-
-export function getFormattedDate(string){
-        return `${string.slice(4)} , ${string.slice(0,3)}`
+export function getFormattedDate(string) {
+  return `${string.slice(4)} , ${string.slice(0, 3)}`;
 }
-
 
 const b = NativeModules;
-const {Alarm }= NativeModules;
-console.log('====================================');
+const { Alarm } = NativeModules;
+console.log("====================================");
 console.log(b);
-console.log('====================================');
+console.log("====================================");
 console.log("++++++++++++++++++++++++++++++++++++");
 console.log(Alarm);
-console.log('====================================');
+console.log("====================================");
 
+export function bunchofAlarm() {
+  // give this func a json object ; perform operation to get
+  // date and time of both in same order and the use a for loop to set multiple alarms
+  // after frequency
 
-export function bunchofAlarm(){
-
-// give this func a json object ; perform operation to get 
-// date and time of both in same order and the use a for loop to set multiple alarms 
-// after frequency
-
-
-const alarm = {
-    alarm_time: '11:40:00 2024-02-13',   // HH:mm:00 yyyy-MM-dd
-    alarm_title: 'alarm-title',
-    alarm_text: 'text',
-    alarm_sound: 'sound',   // sound.mp3
-    alarm_icon: 'icon',     // icon.png
+  const alarm = {
+    alarm_time: "16-30-00 2024-02-20", // HH:mm:00 yyyy-MM-dd
+    alarm_title: "alarm-title",
+    alarm_text: "alarm text",
+    alarm_sound: "cyberalarmssynthesized116358", // sound.mp3
+    alarm_icon: "icon", // icon.png
     alarm_sound_loop: true,
     alarm_vibration: true,
     alarm_noti_removable: true,
-    alarm_activate: true
+    alarm_activate: true,
   };
-  
+
   Alarm.schedule(
     alarm,
-    success => console.log(success),  // success message
-    fail => {console.log(fail);console.log("failed");}         // fail message
-    );
-    
-    Alarm.searchAll(
-      success => console.log(success),  // alarm list
-      fail => console.log(fail)         // fail message
-    );
+    (success) => console.log(success), // success message
+    (fail) => {
+      console.log(fail);
+      console.log("failed");
+    }, // fail message
+  );
+
+  //   Alarm.searchAll(
+  //     (success) => console.log(success), // alarm list
+  //     (fail) => console.log(fail), // fail message
+  //   );
 }
 
 // const AlarmService = NativeModules.AlarmService;
